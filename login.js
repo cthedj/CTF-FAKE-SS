277ecbda5ffbef2398281f5fc06f581dbcdebe9a
cthedj <114969577+cthedj@users.noreply.github.com>
2025-05-08 10:28:13 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,17 @@
+// login.js
 function login(event) {
   event.preventDefault();
 
   const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value;
 
-  // Start timer
   if (!sessionStorage.getItem('startTime')) {
     sessionStorage.setItem('startTime', Date.now());
   }
 
-  // Store username for future use
   sessionStorage.setItem('username', username);
 
-  // Validate both username and password
-  if (username === "AdminGOAT" && password === "dolos2024") {
+  if (username === secret.username && password === secret.password) {
     sessionStorage.setItem('endTime', Date.now());
     window.location.href = "success.html";
   } else {
